aeea5bd59addd240b23f6a91f6c4579fa518243c
sakibatreus <sakibatreus@gmail.com>
2025-08-18 11:00:22 +0600

    chore: get report by auth to params changed

--- src/modules/report/report.route.ts
+++ src/modules/report/report.route.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { getMonthlyReport } from './report.controller';
-import auth from '../../middleware/auth';
-import { userRole } from '../../constants';
 
 const reportRoutes = Router();
 
-reportRoutes.get('/monthly', auth([userRole.admin, userRole.user]), getMonthlyReport);
+reportRoutes.get('/monthly', getMonthlyReport);
 
 export default reportRoutes;
